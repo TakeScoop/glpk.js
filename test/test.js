5e994c2009a19e2659bb090d814658b4bbce6bca
Saeid Rasulkhani <saeid@takescoop.com>
2020-07-31 12:25:23 -0700

    organizing tests, make test for added settings

--- test/test.js
+++ test/test.js
@@ -1,42 +1,60 @@
-const tape = require('tape');
-const fs = require('fs');
-const almostEqual = require('almost-equal');
-const glpk = require('glpk');
-require('../glpk.js').then(glpkjs => {
+'use strict'
 
-	tape('test LP/MIP & compare against native node-glpk', { timeout: 99999 }, t => {
-		[
-			'lp',
-			'diet_large',
-			'mip',
-			'mip2'
-		].map(d => {
 
-			let json = JSON.parse(fs.readFileSync(`${__dirname}/data/${d}.json`).toString());
-			let prob = new glpk.Problem();
-			const settings = json.settings
-			let z1, z2 = glpkjs.solve(json, settings).result.z;
+const _ = require('lodash')
+const fs = require('fs')
+const glpk = require('glpk')
 
-			prob.readLpSync(`${__dirname}/data/${d}.lp`);
-			prob.scaleSync(glpk.SF_AUTO);
-			prob.simplexSync({
-				presolve: glpk.ON,
-				msgLev: glpk.MSG_ERR
-			});
-			if (prob.getNumInt() > 0) {
-				prob.intoptSync();
-				z1 = prob.mipObjVal();
-			} else {
-				z1 = prob.getObjVal()
-			}
-			prob.delete();
+const glpkjs = require('../glpk.js')
 
-			return [z1, z2, d];
+describe('test the solver', function() {
+    
+    it('Solver should have similar results to the node-glpk', function() {
+        return glpkjs.then(glpkjs => {
+        
+            const problemInstances = [
+                'lp',
+                'diet_large',
+                'mip',
+                'mip2'
+            ]
+            _.map(problemInstances, instance => {
+                const json = JSON.parse(fs.readFileSync(`${__dirname}/data/${instance}.json`).toString())
+                
+                const prob = new glpk.Problem()
+                prob.readLpSync(`${__dirname}/data/${instance}.lp`)
+                prob.scaleSync(glpk.SF_AUTO)
+                prob.simplexSync({
+                    presolve: glpk.ON,
+                    msgLev: glpk.MSG_ERR
+                })
+                let glpkObjValue
+                if (prob.getNumInt() > 0) {
+                    prob.intoptSync()
+                    glpkObjValue = prob.mipObjVal()
+                } else {
+                    glpkObjValue = prob.getObjVal()
+                }
+                prob.delete()
+                
+                const settings = json.settings
+                const glpkJsSol = glpkjs.solve(json, settings)
+                expect(_.round(glpkJsSol.result.z, 2)).to.be.equal(_.round(glpkObjValue, 2))
+            })
+        })
+    })
 
-		}).forEach(z => {
-			t.ok(almostEqual(z[0], z[1], almostEqual.FLT_EPSILON), z[2]);
-		})
-		t.end();
-	});
-
-});
+    it('The time limit should kill the solver before finding optimal solution', function() {
+        const problem = 'mip2'
+        const json = JSON.parse(fs.readFileSync(`${__dirname}/data/${problem}.json`).toString())
+        const settings = _.defaults({tmLim: 1}, json.settings)
+        
+        
+        return glpkjs.then(glpkjs => {
+            const sol = glpkjs.solve(json, settings)
+            expect(sol.result.status).be.equal(1)
+            expect(sol.result.vars.x1).be.equal(0)
+            expect(sol.result.z).be.equal(0)
+        })
+    })
+})
