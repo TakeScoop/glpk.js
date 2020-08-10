f860db84269a676fd34b3a40414a207769615900
Saeid Rasulkhani <saeid@takescoop.com>
2020-08-10 12:39:41 -0700

    return tests to be in tape

--- test/test.js
+++ test/test.js
@@ -1,60 +1,57 @@
-'use strict'
-
-
 const _ = require('lodash')
-const fs = require('fs')
-const glpk = require('glpk')
+const tape = require('tape');
+const fs = require('fs');
+const almostEqual = require('almost-equal');
+const glpk = require('glpk');
+require('../glpk.js').then(glpkjs => {
 
-const glpkjs = require('../glpk.js')
+	tape('test LP/MIP & compare against native node-glpk', { timeout: 99999 }, t => {
+		[
+			'lp',
+			'diet_large',
+			'mip',
+			'mip2'
+		].map(d => {
 
-describe('test the solver', function() {
+			let json = JSON.parse(fs.readFileSync(`${__dirname}/data/${d}.json`).toString());
+			let prob = new glpk.Problem();
+			const settings = json.settings
+			let z1, z2 = glpkjs.solve(json, settings).result.z;
+
+			prob.readLpSync(`${__dirname}/data/${d}.lp`);
+			prob.scaleSync(glpk.SF_AUTO);
+			prob.simplexSync({
+				presolve: glpk.ON,
+				msgLev: glpk.MSG_ERR
+			});
+			if (prob.getNumInt() > 0) {
+				prob.intoptSync();
+				z1 = prob.mipObjVal();
+			} else {
+				z1 = prob.getObjVal()
+			}
+			prob.delete();
+
+			return [z1, z2, d];
+
+		}).forEach(z => {
+			t.ok(almostEqual(z[0], z[1], almostEqual.FLT_EPSILON), z[2]);
+		})
+		t.end();
+	});
+  
+  tape('The time limit should kill the solver before finding optimal solution', { timeout: 99999 }, t => {
+    const problem = 'mip2'
+    let json = JSON.parse(fs.readFileSync(`${__dirname}/data/${problem}.json`).toString());
+    const settings = _.defaults({tmLim: 1}, json.settings)
     
-    it('Solver should have similar results to the node-glpk', function() {
-        return glpkjs.then(glpkjs => {
-        
-            const problemInstances = [
-                'lp',
-                'diet_large',
-                'mip',
-                'mip2'
-            ]
-            _.map(problemInstances, instance => {
-                const json = JSON.parse(fs.readFileSync(`${__dirname}/data/${instance}.json`).toString())
-                
-                const prob = new glpk.Problem()
-                prob.readLpSync(`${__dirname}/data/${instance}.lp`)
-                prob.scaleSync(glpk.SF_AUTO)
-                prob.simplexSync({
-                    presolve: glpk.ON,
-                    msgLev: glpk.MSG_ERR
-                })
-                let glpkObjValue
-                if (prob.getNumInt() > 0) {
-                    prob.intoptSync()
-                    glpkObjValue = prob.mipObjVal()
-                } else {
-                    glpkObjValue = prob.getObjVal()
-                }
-                prob.delete()
-                
-                const settings = json.settings
-                const glpkJsSol = glpkjs.solve(json, settings)
-                expect(_.round(glpkJsSol.result.z, 2)).to.be.equal(_.round(glpkObjValue, 2))
-            })
-        })
-    })
+    const sol = glpkjs.solve(json, settings);
 
-    it('The time limit should kill the solver before finding optimal solution', function() {
-        const problem = 'mip2'
-        const json = JSON.parse(fs.readFileSync(`${__dirname}/data/${problem}.json`).toString())
-        const settings = _.defaults({tmLim: 1}, json.settings)
-        
-        
-        return glpkjs.then(glpkjs => {
-            const sol = glpkjs.solve(json, settings)
-            expect(sol.result.status).be.equal(1)
-            expect(sol.result.vars.x1).be.equal(0)
-            expect(sol.result.z).be.equal(0)
-        })
-    })
-})
+    t.equal(1, sol.result.status, 'solution is undefined')
+    t.equal(0, sol.result.vars.x1, 'the variable has the value of zero')
+    t.equal(0, sol.result.z, 'objective function is zero')
+    
+    t.end();
+  });
+
+});
